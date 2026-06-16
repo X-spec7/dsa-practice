9f616ce5d7274d05784c7ebd2d65b5a159b27bb8
X-spec7 <mercury.spec77@gmail.com>
2026-06-16 12:19:34 +0900

    feat: implement space optimized version

--- algorithm/dp/matrix/unique_path.py
+++ algorithm/dp/matrix/unique_path.py
@@ -7,6 +7,15 @@ def uniquePaths(m: int, n: int) -> int:
     
     return dp[m-1][n-1]
 
+def uniquePathsSpaceOptimized(m: int, n: int) -> int:
+    dp = [1] * n
+    
+    for row in (1, m):
+        for col in range(1, n):
+            dp[col] = dp[col] + dp[col - 1]
+    
+    return dp[n - 1]
+
 while True:
     try:
         m = int(input("Please input the number of the rows of the matrix: "))
@@ -22,6 +31,8 @@ while True:
         print("Please input a valid number.")
         
 pathCount = uniquePaths(m, n)
+pathCountSpaceOptimized = uniquePathsSpaceOptimized(m, n)
 
 print("The count of all possible unique path is: ", pathCount)
+print("The count of all possible unique path counted by space optimized version is: ", pathCountSpaceOptimized)
     
